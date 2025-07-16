202d715df26a3f3eb1c5ab9e90f09ce6da21d5be
Ahmad Abdullatif <AhmedAbdellatef3846@gmail.com>
2025-07-16 14:29:49 +0300

    fix long break bug

--- pomodoro_timer/main.py
+++ pomodoro_timer/main.py
@@ -14,7 +14,6 @@ from playsound import playsound
 
 
 CYCLES     = 4
-LONG_BREAK = 20
 notify2.init('Pomodoro timer')
 work_message = notify2.Notification("Pomodoro", "Time to work!")
 short_break_message = notify2.Notification("Pomodoro", "Take a break!")
@@ -48,11 +47,11 @@ def countdown(time_in_secs,title):
               , help = 'work duration in minutes', type = int)
 @click.option('-b','--break-time',default = 5
               , help = 'break duration in minutes', type = int)
-@click.option('-r','--rounds',default = 2
-              , help = 'the number of rounds', type = int)
 @click.option('-l','--long-break',default = 20
               ,help = 'long break duration in minutes', type = int)
-def main(work_time,break_time,rounds):
+@click.option('-r','--rounds',default = 2
+              , help = 'the number of rounds', type = int)
+def main(work_time,break_time,long_break,rounds):
     """
     Runs a Pomodoro timer loop with specified work time, break time
     , and number of rounds.
@@ -67,6 +66,7 @@ def main(work_time,break_time,rounds):
     while round_counter < rounds:
         work_message.show()
         playsound(path_to_sound)
+        print('--------------------------------')
         countdown(work_time*60,f'session {session_counter}')
         if session_counter < CYCLES:
             short_break_message.show()
@@ -76,7 +76,7 @@ def main(work_time,break_time,rounds):
         else:
             long_break_message.show()
             playsound(path_to_sound)
-            countdown(LONG_BREAK*60,'long break')
+            countdown(long_break*60,'long break')
             round_counter += 1
             session_counter = 1
     print("you're done for today!, good job")
