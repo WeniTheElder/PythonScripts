5082ccf12e2480b7a84cd9caa6647d80d248008e
Ahmad Abdullatif <AhmedAbdellatef3846@gmail.com>
2025-07-19 18:12:38 +0300

    fix quit bug in simple TCP server

--- simple TCP server/client2.py
+++ simple TCP server/client2.py
@@ -5,7 +5,7 @@ PORT = 2000
 with socket.socket() as client:
     client.connect(('localhost', PORT))
     while True:
-        message = input("enter a message to send:").encode()
-        if message.lower() == 'q':
+        message = input("enter a message to send('quit' to exit):")
+        if message.lower() == 'quit':
             break
-        client.sendall(message)
+        client.sendall(message.encode())
